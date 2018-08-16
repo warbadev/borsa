126fb1ef0cf2692d5b01a5e4056a95cac43578aa
abdullah <warbadev@gmail.com>
2018-08-16 21:53:30 +0300

    l
    k

--- server.js
+++ server.js
@@ -79,11 +79,9 @@ let upload = multer({
   next();
 }); 
 app.use('/uploads', express.static(process.cwd() + '/uploads'));
-app.use(express.static(path.join(__dirname, '/dist')));
-app.use(express.static('/dist'));
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, './dist')));
 
-//app.get('*',(req,res) => res.sendFile(path.join(__dirname+'/dist/index.html')));
+app.get('*',(req,res) => res.sendFile(path.join(__dirname+'/dist/index.html')));
 app.get('/api', function (req, res) {
   res.end('file catcher example');
 });

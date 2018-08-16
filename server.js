416387a3b5b2a5f4c56b1dad1ea3acae2adca545
abdullah <warbadev@gmail.com>
2018-08-16 18:52:50 +0300

    checking configslsssxwaasaaggazsasal

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ let upload = multer({
  app.use(function (req, res, next) {
   //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200/');
   
-
+  req.forwardedSecure = (req.headers["x-forwarded-proto"] == "https");
 
   //res.setHeader('Access-Control-Allow-Methods', 'POST');
   //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');

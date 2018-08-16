bc6ae311f065e9c002ee13eddfba65d81f8d871a
abdullah <warbadev@gmail.com>
2018-08-16 18:23:16 +0300

    checking configslsssxwaasaaggazs

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ let upload = multer({
 app.use('/uploads', express.static(process.cwd() + '/uploads'));
 app.use(express.static(path.join(__dirname, '/dist/')));
 
-app.get('*',(req,res) => res.sendFile(path.join(__dirname+'/dist/index.html')));
+app.get('*',(req,res) => res.sendFile(path.join(__dirname+'/dist/')));
 app.get('/api', function (req, res) {
   res.end('file catcher example');
 });
